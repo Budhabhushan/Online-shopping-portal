a041db8189c6b8993aca887d431e5d5e36553e7f
Budhabhushan <jawalebhushan77@gmail.com>
2024-03-19 14:37:23 +0530

    all the new changes

--- util/database.js
+++ util/database.js
@@ -1,10 +1,20 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-development',
-    password:'admin@root'
-})
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-development',
+//     password:'admin@root'
+// })
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+
+const Sequelize =  require('sequelize');
+
+const sequelize = new Sequelize('node-development','root','admin@root',{
+   dialect:'mysql',
+   host:'localhost'
+});
+
+module.exports = sequelize;
