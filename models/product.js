403a47c77d97323a4a6563d2c9b01b89d45bcc12
Budhabhushan <jawalebhushan77@gmail.com>
2024-03-23 13:41:09 +0530

    New Files

--- models/product.js
+++ models/product.js
@@ -1,59 +1,42 @@
+// const Sequelize = require('sequelize');
 
-// const db = require('../util/database');
+// const sequelize = require('../util/database');
 
-// const Cart = require('./cart');
+const getDb = require('../util/database').getDb;
 
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
+class Product {
+   constructor(title,price,imageUrl,description) {
+     this.title = title,
+     this.price = price;
+     this.imageUrl = imageUrl,
+     this.description = description
+   }
 
-//   save() {
-//     return db.execute('INSERT INTO products (title,price, imageUrl, description) VALUES (?, ?, ?, ?)',
-//     [this.title, this.price, this.imageUrl, this.description]);
-//   }
+   save(){
 
-//   static deleteById(id) {
+   }
+}
 
-//   }
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM products');
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-//   }
-// };
-
-const Sequelize = require("sequelize");
-
-const sequelize = require("../util/database");
-
-const Product = sequelize.define("product", {
+const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
-    primaryKey: true,
+    primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
-  },
+    allowNull: false
+  }
 });
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -10,11 +10,38 @@
 // module.exports = pool.promise();
 
 
-const Sequelize =  require('sequelize');
+// const Sequelize =  require('sequelize');
 
-const sequelize = new Sequelize('node-development','root','admin@root',{
-   dialect:'mysql',
-   host:'localhost'
-});
+// const sequelize = new Sequelize('shopping-portal','root','admin@root',{
+//    dialect:'mysql',
+//    host:'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+const mongoConnect = (cb) =>{
+   MongoClient.connect(
+      'mongodb+srv://Bhushan:Bhushan%40123@cluster0.hhyq8d5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+   ).then(client =>{
+    console.log('Connect!');
+    _db = client.db();
+    cb();
+   }).catch(err => {
+    console.log(err);
+    throw err
+   })
+}
+
+const getDb = () =>{
+   if(_db) {
+      return _db;
+   }
+   throw 'No Database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
